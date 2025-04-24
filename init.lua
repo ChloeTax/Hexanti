73383d960f11225ffddb1f4507085234011bf780
chloetax <overratedchloe@gmail.com>
2025-04-24 11:41:21 -0700

    update names from hexcasting to hexanti

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ dofile("./hexEmulator/hexEmulator.lua")
 
 
 core.register_on_player_receive_fields(function(player, formname, fields)
-    if formname ~= "hexcasting:hexInput" then
+    if formname ~= "hexanti:hexInput" then
         return
     end
 
@@ -21,7 +21,7 @@ end)
 function trinket(itemstack, user, pointed_thing)
 
     if user:get_player_control().sneak then
-        
+
         local formspec = {
             "formspec_version[4]",
             "size[24,24]",
@@ -30,16 +30,16 @@ function trinket(itemstack, user, pointed_thing)
             "button[20,0.25;3,0.8;save;Save]"
         }
 
-        core.show_formspec(user:get_player_name(), "hexcasting:hexInput", table.concat(formspec, ""))
+        core.show_formspec(user:get_player_name(), "hexanti:hexInput", table.concat(formspec, ""))
 
     else
         Hexcasting.castHex(itemstack:get_meta():get_string("hex"), user)
     end
 end
 
-core.register_craftitem("hexcasting:trinket", {
+core.register_craftitem("hexanti:trinket", {
     description = "casts a stored hex",
-    inventory_image = "hexcasting_trinket.png",
+    inventory_image = "hexanti_trinket.png",
     stack_max = 1,
     on_use = trinket,
 })
